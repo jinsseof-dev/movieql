db02d32fb34dcf02f181c5de138cb7d1e6e615c2
jinsseof <jinsseof@jiran.com>
2021-06-20 22:49:50 +0900

    id column type change

--- graphql/db.js
+++ graphql/db.js
@@ -1,31 +1,31 @@
 let movies = [
     {
-        id: "1",
+        id: 1,
         name: "Nicolas",
         score: 18
     },
     {
-        id: "2",
+        id: 2,
         name: "Dall",
         score: 19
     },
     {
-        id: "3",
+        id: 3,
         name: "JD",
         score: 17
     },
     {
-        id: "4",
+        id: 4,
         name: "flynn",
         score: 18
     },
     {
-        id: "5",
+        id: 5,
         name: "Jeff",
         score: 20
     },
     {
-        id: "6",
+        id: 6,
         name: "bernet",
         score: 21
     }
@@ -34,13 +34,13 @@ let movies = [
 export const getMovies = () => movies;
 
 export const getById = id => {
-    const filteredMovies = movies.filter(movie => movie.id === String(id));
+    const filteredMovies = movies.filter(movie => movie.id === id);
     return filteredMovies[0];
 }
 
 
 export const deleteMovie = (id) => {
-    const cleanedMovies = movies.filter(movie => movie.id !== String(id));
+    const cleanedMovies = movies.filter(movie => movie.id !== id);
     if (movies.length > cleanedMovies.length) {
         movies = cleanedMovies;
         return true;
@@ -51,7 +51,7 @@ export const deleteMovie = (id) => {
 
 export const addMovie = (name, score) => {
     const newMovie = {
-        id: `${movies.length + 1}`,
+        id: movies.length + 1,
         name,
         score
     };
